a8b59d62cdc18bbe20ef7b7af2bcaed6e9a490fa
Leonardo Espejo <leonardocesarespejo@gmail.com>
2023-06-28 09:54:59 -0500

    SucursalInterfaz y SucursalDAO

---
+++ src/Interfaces/SucursalInterfaz.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interfaces;
+
+import Modelo.Sucursal;
+
+/**
+ *
+ * @author Leo
+ */
+public interface SucursalInterfaz extends CRUD<Sucursal>{
+    
+}
---
+++ src/modeloDAO/SucursalDAO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modeloDAO;
+
+import Interfaces.SucursalInterfaz;
+import Modelo.Sucursal;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Leo
+ */
+public class SucursalDAO implements SucursalInterfaz{
+
+    @Override
+    public boolean crear(Sucursal c) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean actualizar(Sucursal a) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(Sucursal e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Sucursal> buscarTodo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Sucursal buscarUno(Sucursal b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
